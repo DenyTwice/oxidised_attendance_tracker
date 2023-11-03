48db5cafafd03caf83283e0c01a8f3092e1f2bba
DenyTwice <ivinjabraham@gmail.com>
2023-11-03 15:16:55 +0530

    adds simple query and insert operation

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,60 @@
-mod schema;
-use crate::schema::attendee;
-use crate::schema::event;
-use diesel::prelude::*;
-use serde::{Serialize, Deserialize};
-use serde_json::Value as JsonValue;
+use std::env;
 
-#[derive(Queryable, Serialize, Deserialize, Insertable)]
-#[diesel(table_name = event)]
-pub struct Event {
-    pub event_name: String,
-    pub starting_date: String, // Consider using chrono::NaiveDate if you want to handle it as a date
-    pub number_of_days: i32,
-    pub number_of_sessions: i32,
+use diesel::prelude::*;
+use diesel::PgConnection;
+use dotenv::dotenv;
+use schema::event;
+
+mod schema;
+
+#[derive(Queryable)]
+struct EventEntity {
+    event_name: String,
+    starting_date: Option<String>, // Consider using chrono::NaiveDate if you want to handle it as a date
+    number_of_days: Option<i32>,
+    number_of_sessions: Option<i32>,
 }
 
+#[derive(Insertable)]
+#[diesel(table_name = event)]
+struct NewEvent {
+    event_name: String,
+    starting_date: String,
+    number_of_days: i32,
+    number_of_sessions: i32,
+}
 
-#[derive(Queryable, Serialize, Deserialize, Insertable)]
-#[diesel(table_name = attendee)]
-pub struct Participant {
-    pub id: i32,
-    pub event_name: String,
-    pub name: String,
-    pub email: String,
-    pub roll_number: String,
-    pub attendance_log: JsonValue,
-    pub misc_log: JsonValue,
+fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("Database URL must be set");
+
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+fn main() {
+    let mut connection = establish_connection();
+
+    let new_event = NewEvent {
+        event_name: String::from("Test2"),
+        starting_date: String::from("10-12-2020"),
+        number_of_days: 2,
+        number_of_sessions: 2,
+    };
+
+    diesel::insert_into(event::table)
+        .values(&new_event)
+        .execute(&mut connection)
+        .expect("Error inserting event");
+
+    let results: Vec<EventEntity> = event::table
+        .limit(5)
+        .load::<EventEntity>(&mut connection)
+        .expect("Error loading results from event");
+
+    for entity in results {
+        print!("Found entity {}", entity.event_name);
+    }
 }
